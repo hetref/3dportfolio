cfdc4ec66f7e1abf3964d4329bfe24806d19e261
Aryan Shinde <shindearyan179@gmail.com@gmail.com>
2023-06-06 18:49:17 +0530

    Initial Commit

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./logo.svg";
+
+import backend from "./backend.png";
+import frontend from "./frontend.png";
+import app from "./app.png";
+import reactnative from "./reactnative.png";
+
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import jquery from "./tech/jquery.png";
+import heroku from "./tech/heroku.png";
+import netlify from "./tech/netlify.png";
+import firebase from "./tech/firebase.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  frontend,
+  app,
+  reactnative,
+  github,
+  menu,
+  close,
+  css,
+  jquery,
+  netlify,
+  heroku,
+  figma,
+  firebase,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
